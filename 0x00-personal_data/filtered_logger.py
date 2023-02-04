c45966bd33e7c813183a90932e381a62e6f2a18f
cecilia-89 <catabong89@gmail.com>
2023-02-04 12:36:21 +0100

    filtered_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,6 +11,5 @@ def filter_datum(fields: List[str],
     """returns the log message obfuscated"""
     msg = message
     for field in fields:
-        msg = re.sub(r'({}=)[a-zA-Z0-9/]+{}'.format(field, separator),
-                     r'\1{};'.format(redaction), msg)
+        msg = re.sub(r'({}=)[a-zA-Z0-9/]+{}'.format(field, separator), r'\1{};'.format(redaction), msg)
     return msg
