42d979583a7aa663efdcb8eb6282810fe5ebc85d
cecilia-89 <catabong89@gmail.com>
2023-02-04 20:51:08 +0100

    filterd logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module: filtered_logger.py"""
 from typing import List
+import logging
 import re
 
 
@@ -13,3 +14,21 @@ def filter_datum(fields: List[str],
         message = re.sub(r'({}=).+?{}'.format(field, separator),
                          r'\1{}{}'.format(redaction, separator), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """returns formatted log"""
+        formatted = super().format(record)
+        message = filter_datum(self.fields, self.REDACTION,
+                               formatted, self.SEPARATOR)
+        return message
