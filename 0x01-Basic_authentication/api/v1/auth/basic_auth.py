467e35318e99c8efe3a45610b19820c926722444
cecilia-89 <catabong89@gmail.com>
2023-02-06 22:27:23 +0100

    Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -36,6 +36,7 @@ class BasicAuth(Auth):
     def extract_user_credentials(self,
                                  decoded_base64_authorization_header: str
                                  ) -> Tuple[str, str]:
+        """extracts and returns user credentials"""
         decoded_base = decoded_base64_authorization_header
         if decoded_base is not None and type(decoded_base) == str:
             if ':' in decoded_base:
