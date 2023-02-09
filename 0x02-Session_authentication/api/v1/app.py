162b7d4ea94e0cc73d09ae001fd6c311be056b1f
cecilia-89 <catabong89@gmail.com>
2023-02-09 13:18:37 +0100

    session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -16,9 +16,9 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = Auth()
 
-if getenv("AUTH_TYPE") == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
+if getenv("AUTH_TYPE") == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(401)
@@ -46,12 +46,13 @@ def not_found(error) -> str:
 def setup():
     """first func to execute"""
     exclude_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                     '/api/v1/forbidden/']
+                     '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth is not None:
         is_authenticated = auth.require_auth(request.path, exclude_paths)
 
         if is_authenticated:
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
 
             if auth.current_user(request) is None:
