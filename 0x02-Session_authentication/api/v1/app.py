0441ede20ab5187e47bbc5f0e8bed2047a2bd6fd
cecilia-89 <catabong89@gmail.com>
2023-02-10 13:53:07 +0100

    session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -15,9 +15,9 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = BasicAuth()
 
-if getenv("AUTH_TYPE") == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
+if getenv("AUTH_TYPE") == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
 
 
 @app.errorhandler(401)
---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Module: Session Expiration"""
+from api.v1.auth.session_auth import SessionAuth
+from os import getenv
+from datetime import datetime, timedelta
+
+class SessionExpAuth(SessionAuth):
+    """adds expiration date to a session"""
+
+    def __init__(self):
+        """constructor class"""
+        try:
+            duration = int(getenv('SESSION_DURATION'))
+        except Exception:
+            duration = 0
+        self.session_duration = duration
+
+    def create_session(self, user_id=None):
+        """creates a session id"""
+        session_id = super().create_session(user_id)
+        if session_id:
+            self.user_id_by_session_id[session_id] = "session dictionary"
+            self.user_id_by_session_id["user_id"] = user_id
+            self.user_id_by_session_id["created_at"] = datetime.now()
+            return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """returns user id for session"""
+        session_value = self.user_id_by_session_id.get(session_id)
+        if session_value is None:
+            return None
+
+        if self.session_duration <= 0:
+            return self.user_id_by_session_id['user_id']
+
+        created_at = self.user_id_by_session_id.get('created_at')
+        if created_at:
+            created_at += timedelta(seconds=self.session_duration)
+            if created_at > datetime.now():
+                return self.user_id_by_session_id['user_id']
+        return None
