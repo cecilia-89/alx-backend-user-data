b7a392cafb3d3ffa1950751e600d4815630e0859
cecilia-89 <catabong89@gmail.com>
2023-02-12 00:48:59 +0100

    Basic Authentication

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -13,16 +13,43 @@ class SessionDBAuth(SessionExpAuth):
         if session_id:
             u = UserSession(**{'user_id': user_id, 'session_id': session_id})
             u.save()
+            UserSession.save_to_file()
             return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """returns the User ID"""
         if session_id is None:
             return None
+        UserSession.load_from_file()
         user_obj = UserSession.search({'session_id': session_id})
         if user_obj:
             return user_obj[0].user_id
 
     def destroy_session(self, request=None):
         """destroys a session"""
-        return super().destroy_session(request)
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if not user_id:
+            return False
+
+        user_session = UserSession.search({
+            'session_id': session_id
+        })
+
+        if not user_session:
+            return False
+
+        user_session = user_session[0]
+
+        try:
+            user_session.remove()
+            UserSession.save_to_file()
+        except Exception:
+            return False
--- 0x02-Session_authentication/main.py
+++ 0x02-Session_authentication/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Main 4
 """
-from api.v1.auth.session_exp_auth import SessionExpAuth
-s = SessionExpAuth()
-print(s.user_id_for_session_id('kkkk'))
+from api.v1.auth.session_db_auth import SessionDBAuth
+s = SessionDBAuth()
+s.user_id_for_session_id('39381576-ccdf-4fca-9e55-af8791261a9c')
