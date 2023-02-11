a59a35695099172ea97606614812c3b4dacf276b
cecilia-89 <catabong89@gmail.com>
2023-02-12 00:56:41 +0100

    Basic Authentication

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -2,8 +2,6 @@
 """Module: Session database Expiration"""
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
-from datetime import timedelta
-import datetime
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -19,54 +17,14 @@ class SessionDBAuth(SessionExpAuth):
             return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """returns user_id or None"""
+        """returns the User ID"""
         if session_id is None:
             return None
-
         UserSession.load_from_file()
-        user_session = UserSession.search({
-            'session_id': session_id
-        })
-
-        if not user_session:
-            return None
-
-        user_session = user_session[0]
-
-        expired_time = user_session.created_at + \
-            timedelta(seconds=self.session_duration)
-
-        if expired_time < datetime.utcnow():
-            return None
-
-        return user_session.user_id
-
+        user_obj = UserSession.search({'session_id': session_id})
+        if user_obj:
+            return user_obj[0].user_id
 
     def destroy_session(self, request=None):
         """destroys a session"""
-        if request is None:
-            return False
-
-        session_id = self.session_cookie(request)
-        if session_id is None:
-            return False
-
-        user_id = self.user_id_for_session_id(session_id)
-
-        if not user_id:
-            return False
-
-        user_session = UserSession.search({
-            'session_id': session_id
-        })
-
-        if not user_session:
-            return False
-
-        user_session = user_session[0]
-
-        try:
-            user_session.remove()
-            UserSession.save_to_file()
-        except Exception:
-            return False
+        return super().destroy_session(request)
