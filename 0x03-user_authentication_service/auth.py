553a6f7e344a63fbf957c3ac2d61b31bc1d5adc1
cecilia-89 <catabong89@gmail.com>
2023-02-15 18:42:14 +0100

    added readme

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,8 +44,9 @@ class Auth:
 
     def get_user_from_session_id(self, session_id: str) -> User:
         """returns a user based on the session id"""
-        user = self._db.find_user_by({"session_id": session_id})
-        return user
+        if session_id:
+            user = self._db.find_user_by({"session_id": session_id})
+            return user
 
     def destroy_session(self, user_id: int) -> None:
         """destroys a users's session"""
