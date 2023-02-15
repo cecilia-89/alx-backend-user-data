c29d079679d1de035ffa5ead06c135d6520d0d86
cecilia-89 <catabong89@gmail.com>
2023-02-15 20:30:44 +0100

    added readme

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -48,5 +48,6 @@ class DB:
         for k, v in kwargs.items():
             if hasattr(user, k):
                 user.k = v
+                self._session.commit()
                 return
         raise ValueError
