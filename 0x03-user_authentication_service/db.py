a24b2e0bd026fe8114a353a85be3442aa7fc1159
cecilia-89 <catabong89@gmail.com>
2023-02-15 19:47:52 +0100

    added readme

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,6 +3,7 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 from user import Base, User
@@ -43,11 +44,9 @@ class DB:
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """updates a user based on keyword arguments"""
-        try:
-            user = self.find_user_by(id=user_id)
-            for k, v in kwargs.items():
-                if hasattr(user, k):
-                    user.k = v
-        except NoResultFound:
-            raise ValueError
-
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if hasattr(user, k):
+                user.k = v
+                return
+        raise ValueError
