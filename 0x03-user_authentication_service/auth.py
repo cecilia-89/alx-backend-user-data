c175d6be04c1b6b63fd11abd3edd435832490427
cecilia-89 <catabong89@gmail.com>
2023-02-15 21:44:48 +0100

    added readme

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -51,7 +51,7 @@ class Auth:
             except NoResultFound:
                 return None
 
-    def destroy_session(self, user_id):
+    def destroy_session(self, user_id: int):
         """destroys a users's session"""
         try:
             self._db.update_user(user_id, {"session_id": None})
