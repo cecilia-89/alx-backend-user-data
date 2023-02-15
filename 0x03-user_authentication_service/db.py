26d51675cf2dd870eac36c953fd15cd740acfa0f
cecilia-89 <catabong89@gmail.com>
2023-02-15 12:29:02 +0100

    added readme

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,8 +40,34 @@ class DB:
 
     def find_user_by(self, **kwargs: Dict) -> User:
         """returns user based on the argument"""
-        return self._session.query(User).filter_by(**kwargs).one()
+        user = self._session.query(User).filter_by(**kwargs).one()
+        if user is None:
+            raise NoResultFound
+        for k in kwargs.keys():
+            if hasattr(user, k):
+                return user
+        raise InvalidRequestError
 
     def update_user(self, user_id: int, **kwargs: Dict) -> None:
         """updates a user based on keyword arguments"""
         self.find_user_by(id=user_id).update(kwargs)
+
+my_db = DB()
+
+user = my_db.add_user("test@test.com", "PwdHashed")
+print(user.id)
+
+find_user = my_db.find_user_by(email="test@test.com")
+print(find_user.id)
+
+try:
+    find_user = my_db.find_user_by(email="test2@test.com")
+    print(find_user.id)
+except NoResultFound:
+    print("Not found")
+
+try:
+    find_user = my_db.find_user_by(no_email="test@test.com")
+    print(find_user.id)
+except InvalidRequestError:
+    print("Invalid")    
