4743e698d6d2286b568acb1ef9c4e8086f0f423a
cecilia-89 <catabong89@gmail.com>
2023-02-16 14:19:54 +0100

    user service data

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,11 +72,12 @@ class Auth:
         """updates a user's password"""
         try:
             user = self._db.find_user_by({'reset_token': reset_token})
-            hashed_password = _hash_password(password)
-            param = {'hashed_password': hashed_password, 'reset_token': None}
-            self._db.update_user(user.id, **param)
         except NoResultFound:
             raise ValueError
+
+        hashed_password = _hash_password(password)
+        param = {'hashed_password': hashed_password, 'reset_token': None}
+        self._db.update_user(user.id, **param)
 
 
 def _hash_password(password: str) -> bytes:
