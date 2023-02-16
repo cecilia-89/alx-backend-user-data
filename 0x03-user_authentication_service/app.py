586466a97878ff461cc7c3fb2b6ce3dd2cd6010d
cecilia-89 <catabong89@gmail.com>
2023-02-16 15:40:58 +0100

    user service data

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,7 +66,7 @@ def get_reset_password_token():
     email = request.form.get('email')
     try:
         reset_token = AUTH.get_reset_password_token(email)
-        return jsonify({"email": email, "reset_token": reset_token})
+        return jsonify({"email": email, "reset_token": reset_token}), 200
     except ValueError:
         abort(403)
 
