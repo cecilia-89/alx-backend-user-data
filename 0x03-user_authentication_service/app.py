d9ab1e911ca40b09ee6884040c8f1976a690d4a2
cecilia-89 <catabong89@gmail.com>
2023-02-16 15:13:17 +0100

    user service data

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,10 +65,9 @@ def get_reset_password_token():
     """resets a user"""
     email = request.form.get('email')
     try:
-        AUTH._db.find_user_by(**{"email": email})
         reset_token = AUTH.get_reset_password_token(email)
         return jsonify({"email": email, "reset_token": reset_token})
-    except NoResultFound:
+    except ValueError:
         abort(403)
 
 @app.route('/reset_password', strict_slashes=False, methods=['PUT'])
