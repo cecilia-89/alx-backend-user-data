428a763b8a97ff5a902cadbd80076f8eef4c52d8
cecilia-89 <catabong89@gmail.com>
2023-02-16 18:11:47 +0100

    user service data

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,8 +75,7 @@ class Auth:
         try:
             user = self._db.find_user_by(**{'reset_token': reset_token})
             hashed_password = _hash_password(password)
-            param = {'hashed_password': hashed_password, 'reset_tokn': None}
-            self._db.update_user(user.id, **param)
+            self._db.update_user(user.id, **{'hashed_password': hashed_password, 'reset_token': None})
         except NoResultFound:
             raise ValueError
 
